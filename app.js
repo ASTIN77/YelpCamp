123bef01d905d18e74f71a5f625f33d1c535e6dc
Andrew Tinsley <andrew@astinsley.net>
2023-08-26 15:30:55 +0100

    Updated Mongo Connection and server listening function

--- app.js
+++ app.js
@@ -1,52 +1,74 @@
-var express                 = require("express"),
-    app                     = express(),
-    bodyParser              = require("body-parser"),
-    mongoose                = require("mongoose"),
-    Campground              = require("./models/campground"),
-    Comment                 = require("./models/comment"),
-    User                    = require("./models/user"),
-    passport                = require("passport"),
-    LocalStrategy           = require("passport-local"),
-    passportLocalMongoose   = require("passport-local-mongoose"),
-    methodOverride          = require("method-override"),
-    seedDB                  = require("./seeds"),
-    flash                   = require("connect-flash");
-    mongoose.Promise        = global.Promise;
-    
-var commentsRoutes          = require("./routes/comments"),
-    campgroundRoutes        = require("./routes/campgrounds"),
-    indexRoutes             = require("./routes/index");
-    
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  Campground = require("./models/campground"),
+  Comment = require("./models/comment"),
+  User = require("./models/user"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose"),
+  methodOverride = require("method-override"),
+  seedDB = require("./seeds"),
+  flash = require("connect-flash");
+mongoose.Promise = global.Promise;
+require("dotenv").config();
+
+var commentsRoutes = require("./routes/comments"),
+  campgroundRoutes = require("./routes/campgrounds"),
+  indexRoutes = require("./routes/index");
+
 app.set("view engine", "ejs");
+app.set("port", process.env.PORT || 3000);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
-mongoose.connect(process.env.YELPCAMPDATABASEURL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.YELPCAMPDATABASEURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 //SeedDB();
 // PASSPORT CONFIG
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "TheRealMcCoyCheddarAndOnion",
     resave: false,
-    saveUninitialized: false }));
-    
-    
+    saveUninitialized: false,
+  })
+);
+
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-app.use(function(req,res, next){
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    res.locals.confirm = req.flash("confirm");
-    next(); });
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  res.locals.confirm = req.flash("confirm");
+  next();
+});
 app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentsRoutes);
 
+const connectDB = async () => {
+  try {
+    const conn = mongoose.connect(process.env.YELPCAMPDATABASEURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-app.listen(process.env.PORT, process.env.IP, function() {
-    console.log("Application Server Running");
+connectDB().then(() => {
+  app.listen(app.get("port"), function () {
+    console.log("Successfully listening on " + app.get("port"));
+  });
 });
